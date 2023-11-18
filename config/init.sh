04aa84892bec835a54fec4f11a0f474210b0f59b
我只想看看蓝天 <1207032539@qq.com>
2023-11-18 19:50:17 +0800

    程序优化

--- config/init.sh
+++ config/init.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #更改php代码存放目录的权限，防止出现文件权限问题
 chown -R www.www /home/wwwroot
-#安装文件转换环境
-yum install -y libreoffice-core libreoffice-writer libreoffice-langpack-zh* libreoffice-calc libreoffice-impress
